9e926c6fced2e492f2958696c079e968580ea5a6
gustavo.vargens <gustavo.vargens@prodatamobility.com.br>
2025-12-08 11:17:14 -0300

    add: Identificadores

---
+++ Identificadores/Program.cs
@@ -0,0 +1,30 @@
+﻿// Idenficadores válidos
+string name;
+string lastName;
+string _adress;
+string age123;
+
+// Identificadores inválidos
+//string 1age;
+//string last name;
+//string #class;
+//string void;
+//string valor$total;
+
+// Nomes de variáveis - Camel Case
+string valorTotal;
+string _descontoAplicado;
+
+// Constantes - Maiúsculas
+string Desconto;
+
+
+// Nomes de classes e métodos - Pascal Case
+class Identificadores
+{
+    void ExibirMensagem()
+    {
+        Console.WriteLine("Identificadores em C#");
+    }
+}
+
