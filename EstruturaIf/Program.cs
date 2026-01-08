f3cd80e765e4dc7c77dbba2e9710a054a908311a
GuFerreiraV <gustavoferreiravargens@gmail.com>
2026-01-08 10:09:24 -0300

    new: Condicionais

---
+++ EstruturaIf/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+Console.WriteLine("Executando projeto Estrutura IF");
+
+Console.WriteLine("Informe o valor de x \t");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Informe o valor de y \t");
+int y = Convert.ToInt32(Console.ReadLine());
+
+string result = (x > y) ? "x é maior que y" : "x é menor ou igual a y";
+
+Console.WriteLine(result);
+
+Console.ReadKey();
