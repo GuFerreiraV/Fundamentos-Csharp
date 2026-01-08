903ed4b83e94ef6aba8196cba986f8e9e7f47fc1
GuFerreiraV <gustavoferreiravargens@gmail.com>
2026-01-08 11:40:16 -0300

    Instrução goto

---
+++ EstruturaDeRepeticao/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Instrução Goto e Label");
+///
+/// Esse laço de repeticação não é recomendado para uso em produção, devido aos seguintes problemas:
+/// Código Espaguete -> O maior argumento contra o goto é que ele permite saltar para quase qualquer lugar do código, quebrando o fluxo lógico linear
+/// Dificuldade de Depuração e Testes -> O uso excessivo de goto pode tornar o código difícil de seguir, depurar e testar, especialmente em programas grandes
+
+int count = 1;
+
+// Identificador
+repeat:
+
+Console.WriteLine($"Contador: {count}");
+count++;
+
+if (count <= 10)
+    goto repeat;
+
+Console.WriteLine("Fim do Programa!");
+Console.ReadKey();
