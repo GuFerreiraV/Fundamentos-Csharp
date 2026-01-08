57010185ea5f2b8a73e1a295549d61438d54bd0b
GuFerreiraV <gustavoferreiravargens@gmail.com>
2026-01-08 14:05:21 -0300

    new: Instrução While

---
+++ LacoDeRepeticaoWhile/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Instrução While");
+
+int x = 0;
+while(x < 5)
+{
+    int y = 0;
+    while(y < 5)
+    {
+        Console.Write($"({x},{y}) ");
+        y++;
+    }
+    x++;
+    Console.WriteLine();
+}
+
+Console.WriteLine("Fim do processamento");
+
