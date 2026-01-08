21c2f7294186d86b2ce7c794019c39cfcaf9c634
GuFerreiraV <gustavoferreiravargens@gmail.com>
2026-01-08 10:48:03 -0300

    new: Estrutura If Else

--- EstruturaIfElse/Program.cs
+++ EstruturaIfElse/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Expressão If e Else");
+
+Console.Write("Digite a nota do aluno: ");
+double nota = Convert.ToDouble(Console.ReadLine());
+
+// Jeito tradicional
+if (nota >= 6.0)
+{
+    Console.WriteLine("Aluno aprovado!");
+}
+else
+{
+    Console.WriteLine("Aluno reprovado!");
+}
+
+
+// Jeito simplificado
+string resultado = (nota >= 6.0) ? "Aluno aprovado!" : "Aluno reprovado!";
+Console.WriteLine(resultado);
